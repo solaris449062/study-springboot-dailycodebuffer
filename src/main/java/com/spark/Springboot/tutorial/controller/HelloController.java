a0dae3817e7998a46d2336a66a4621b6d83b3fa9
Sungoh Park <spark449062@gmail.com>
2022-05-27 07:36:07 -0600

    adding config in properties file

--- src/main/java/com/spark/Springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/spark/Springboot/tutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.spark.Springboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello world";
+        return welcomeMessage;
     }
 }
